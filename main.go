3a1e61785214bb12dc221188b69746e610ed32fd
Rob Merrell <rob@dimpixel.com>
2016-07-26 22:30:05 -0600

    Added command line flags

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/robmerrell/gmboy/system"
 )
 
 func main() {
-	sys := system.NewSystem()
+	debug := flag.String("debug", "", "")
+	bootstrap := flag.String("bootstrap", "", "")
+	flag.Usage = usage
+	flag.Parse()
 
-	err := sys.PerformBootstrap("./roms/bootrom.bin")
-	if err != nil {
-		panic(err)
+	romFile := flag.Arg(0)
+	if romFile == "" {
+		flag.Usage()
+		return
 	}
 
-	sys.StartDebugger("./debug.js")
+	sys := system.NewSystem()
 
-	sys.LoadRom("~/tmp/tetris.gb")
+	if *bootstrap != "" {
+		err := sys.PerformBootstrap(*bootstrap)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if *debug != "" {
+		err := sys.StartDebugger(*debug)
+		if err != nil {
+			fmt.Printf("Error loading %s\n", *debug)
+			return
+		}
+	}
+
+	sys.LoadRom(romFile)
 	sys.Run()
+}
+
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println("  gmbody file.gb\n")
+	fmt.Println("  --bootstrap=file.bin   Run the bootstrap process using the specified file. Default is to not bootstrap.")
+	fmt.Println("  --debug=file.js        Start the debugger and evaluate the specified file.")
+	fmt.Println("  --help                 Show this help text.")
 }
